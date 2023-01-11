19e971ec2100dc3b864770b04d3ffc7f23d0fc7f
Claudiano-Flores <claudiano145@gmail.com>
2023-01-11 20:28:57 -0300

    Add project files.

---
+++ 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Programa1 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("Digite um numero: ");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (x >= 0.0) {
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+                Console.Write("Digite Outro Numero: ");
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            }
+            Console.WriteLine("Numero Negativo!!!");
+        }
+    }
+}
